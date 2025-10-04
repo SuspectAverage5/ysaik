709c3eb6925fb6c48d4e0436e9a53a84424e003d
Yann Saikali <yann.saikali@gmail.com>
2025-10-05 00:18:24 +0300

    aaaa

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+var css = "text-shadow: 1px 1px 2px black, 0 0 1em blue, 0 0 0.2em blue; font-size: 40px;"
+console.log("%cHello there!", css);
+console.log("%cPlease do close this page if you don't know what you're doing.\n", 'color: green; font-size: larger');
